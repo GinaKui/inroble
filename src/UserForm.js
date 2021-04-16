1553107b507c283eaa529e663908d28fc31030d0
ginakui <developerkui@outlook.com>
2021-04-16 11:16:53 -0400

    end of the test

--- src/UserForm.js
+++ src/UserForm.js
@@ -1,7 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
-import { findByLabelText } from '@testing-library/dom';
+import FormControl from "@material-ui/core/FormControl"
+import InputLabel from "@material-ui/core/InputLabel";
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -9,6 +12,10 @@ const useStyles = makeStyles((theme) => ({
       margin: theme.spacing(1),
       display: 'flex'
     },
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
   },
 }));
 
@@ -43,27 +50,39 @@ export default function BasicTextFields() {
         })
       })
       const userData = await res.json();
-      setFirstName(userData.name.firstName);
-      setLastName(userData.name.lastName)
+      setFirstName(userData.name.first);
+      setLastName(userData.name.last);
+      setEmail(userData.email);
+      setVehicle(userData.vehicle);
     }
   },
   [countryCode, phone])
 
   return (
     <form className={classes.root} noValidate autoComplete="off">
-      <div>
+      
         Phone
+      <div>
         <TextField id="outlined-basic" label="country" value={countryCode} variant="outlined" onChange={handleCountryCodeChange}/>
         <TextField id="outlined-basic" label="Phone" value={phone} variant="outlined" onChange={handlePhoneChange}/>
       </div>
       
       <div>
-        Name
-        <TextField id="outlined-basic" label="FirstName" value={phone} variant="outlined" />
-        <TextField id="outlined-basic" label="LastName" value={phone} variant="outlined" />
+        <FormControl className={classes.formControl}>
+        <InputLabel id="userName">Name</InputLabel>
+        <Select
+          labelId="userName"
+          id="userName"
+          value={{firstName, lastName}}
+          onChange={e => {}}
+        >
+          <MenuItem value={{firstName, lastName}}><span>{firstName}</span>{lastName}</MenuItem>
+        </Select>
+      </FormControl>
       </div>
       <TextField id="outlined-basic" label="email" placeholder="Please input your email" value={email} variant="outlined" />
       <TextField id="outlined-basic" label="vehicle" value={vehicle} variant="outlined" />
+      
     </form>
   );
 }
